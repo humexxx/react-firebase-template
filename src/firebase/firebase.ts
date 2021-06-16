501d9f76403077d5d6e65f388b6d8459480ed404
Jason Hume <jahume92@gmail.com>
2021-06-15 23:19:21 -0600

    first commit

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,8 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-// firebase config can be found in your firebase project
-const firebaseConfig = {}
+const firebaseConfig = {
+  apiKey: 'AIzaSyADkImh0Geunj7TN5cSbgkg2EVOwMq5G9c',
+  authDomain: 'medicos-de.firebaseapp.com',
+  projectId: 'medicos-de',
+  storageBucket: 'medicos-de.appspot.com',
+  messagingSenderId: '335867679281',
+  appId: '1:335867679281:web:40b70729c16f7d9e813b10'
+}
 
 firebase.initializeApp(firebaseConfig)
 if (process.env.NODE_ENV !== 'production') {
